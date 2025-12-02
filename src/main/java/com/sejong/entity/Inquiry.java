65cb36ddba1ea12194b6b3de3291ee765462c6ab
choichanwoo001 <114642968+choichanwoo001@users.noreply.github.com>
2025-12-03 01:00:33 +0900

    fix: 문의 이 이동 문제 수정

--- src/main/java/com/sejong/entity/Inquiry.java
+++ src/main/java/com/sejong/entity/Inquiry.java
@@ -11,24 +11,25 @@ import java.util.List;
 @Getter
 @Setter
 public class Inquiry extends BaseEntity {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "inquiry_id")
     private Integer inquiryId;
-    
+
     @Column(name = "name", nullable = false, length = 100)
     private String name;
-    
+
     @Column(name = "email", length = 100)
     private String email;
-    
+
     @Column(name = "message", nullable = false, columnDefinition = "TEXT")
     private String message;
-    
+
     @Column(name = "is_public", nullable = false)
     private Boolean isPublic = true; // 기본값은 공개
-    
+
     @OneToMany(mappedBy = "inquiry", cascade = CascadeType.ALL)
+    @org.hibernate.annotations.BatchSize(size = 100)
     private List<Comment> comments;
-} 
+}
